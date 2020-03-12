5aa70c4facaadfefd61f68ea278bdc7d430a2cf3
deng_glb <dengyongchao_glb@outlook.com>
2020-03-12 20:08:06 +0800

    创建服务消费者工程

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        //使用RestTemplate远程调用 8001提供者服务
+        CommonResult commonResult = restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        return commonResult;
+    }
+
+    @GetMapping("/comsumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id")Long id){
+        CommonResult result = restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        return result;
+    }
+
+}
