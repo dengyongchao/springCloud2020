b553ffd89f0672d0f7f9584ef28f4bbfdf2910f9
deng_glb <dengyongchao_glb@outlook.com>
2020-03-15 17:26:26 +0800

    consul作为注册中心的服务消费者测试成功

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,12 +1,17 @@
 package com.atguigu.springcloud.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -19,26 +24,43 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Autowired
+    private DiscoveryClient discoveryClient; //获取服信息
+
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment){
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
-        System.out.println("*****插入结果："+result);
-        if(result>0){
-            return  new CommonResult(200,"插入成功");
-        }else{
-            return new CommonResult(404,"插入失败",null);
+        System.out.println("*****插入结果：" + result);
+        if (result > 0) {
+            return new CommonResult(200, "插入成功");
+        } else {
+            return new CommonResult(404, "插入失败", null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
-        System.out.println("*****查询结果："+payment);
-        if(payment!=null){
-            return  new CommonResult(200,"查询成功"+serverPort,payment);
-        }else{
-            return new CommonResult(404,"查询失败",null);
+        System.out.println("*****查询结果：" + payment);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功" + serverPort, payment);
+        } else {
+            return new CommonResult(404, "查询失败", null);
         }
     }
 
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        //获取服务信息列表
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            System.out.println(service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for(ServiceInstance instance:instances){
+            System.out.println(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
 }
